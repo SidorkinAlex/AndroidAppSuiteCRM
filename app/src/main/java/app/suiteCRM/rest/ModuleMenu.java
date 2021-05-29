3b5a8bd7ca54df01f4310ed7af3191660f616678
SidorkinAlex <palach.orl@mail.ru>
2021-05-29 23:52:58 +0300

    addet class from parse result moduleList requests

---
+++ app/src/main/java/app/suiteCRM/rest/ModuleMenu.java
@@ -0,0 +1,15 @@
+package app.suiteCRM.rest;
+
+import java.util.HashMap;
+
+public class ModuleMenu {
+    public String id;
+    public String name;
+    public String apiName;
+
+    public ModuleMenu(String id, String name, String apiName) {
+        this.id = id;
+        this.name = name;
+        this.apiName = apiName;
+    }
+}
---
+++ app/src/main/java/app/suiteCRM/rest/dataClasses/GetEntryDataParser.java
@@ -0,0 +1,30 @@
+package app.suiteCRM.rest.dataClasses;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.suiteCRM.rest.ModuleMenu;
+
+public class GetEntryDataParser {
+    public ArrayList<ModuleMenu> parseEntryList(String queryResult) throws JSONException {
+        JSONObject jsonObject = new JSONObject(queryResult);
+        JSONArray entryList = jsonObject.getJSONArray("entry_list");
+        ArrayList<ModuleMenu> moduleMenuCollections = new ArrayList<ModuleMenu>();
+        for (int i=0; i < entryList.length();i++) {
+            JSONObject oneModuleDataJSON = entryList.getJSONObject(i);
+            String id = getProperty(oneModuleDataJSON,"id");
+            String name = getProperty(oneModuleDataJSON,"name");
+            String apiName = getProperty(oneModuleDataJSON,"name");
+            ModuleMenu moduleMenu = new ModuleMenu(id,name,apiName);
+            moduleMenuCollections.add(moduleMenu);
+        }
+        return moduleMenuCollections;
+    }
+    public String getProperty(JSONObject moduleData,String key) throws JSONException {
+        return moduleData.getJSONObject(key).getString(key);
+    }
+}
